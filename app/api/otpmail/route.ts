d71e9f7dae34fc5b5c97c8433711daa0a98a4e28
Pratham mehta <mehtapratham49@gmail.com>
2024-12-06 15:22:35 +0530

    change

--- app/api/otpmail/route.ts
+++ app/api/otpmail/route.ts
@@ -1,24 +1,24 @@
-import { render } from "@react-email/render";
-import { Resend } from "resend";
-import { NextRequest, NextResponse } from "next/server";
-import sendOtpEmail from "@/emails/otp";
+// import { render } from "@react-email/render";
+// import { Resend } from "resend";
+// import { NextRequest, NextResponse } from "next/server";
+// import sendOtpEmail from "@/emails/otp";
 
-const resend = new Resend(process.env.RESEND_KEY!);
+// const resend = new Resend(process.env.RESEND_KEY!);
 
-export const POST = async (req: NextRequest) => {
-  const { email, otp } = await req.json();
+// export const POST = async (req: NextRequest) => {
+//   const { email, otp } = await req.json();
 
-  const {  error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: [email],
-    subject: "hello world",
-    html: await render(
-      sendOtpEmail({
-        otp,
-      })
-    ),
-  });
-  if (error) return NextResponse.json({ messge: error }, { status: 401 });
+//   const {  error } = await resend.emails.send({
+//     from: "Acme <onboarding@resend.dev>",
+//     to: [email],
+//     subject: "hello world",
+//     html: await render(
+//       sendOtpEmail({
+//         otp,
+//       })
+//     ),
+//   });
+//   if (error) return NextResponse.json({ messge: error }, { status: 401 });
 
-  return NextResponse.json({ message: "sent successfully" }, { status: 200 });
-};
+//   return NextResponse.json({ message: "sent successfully" }, { status: 200 });
+// };
