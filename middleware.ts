4d250d6f7da8dfbdf8c12e936a9a8e3864142734
Pratham mehta <mehtapratham49@gmail.com>
2024-12-07 17:01:49 +0530

    added-the-protected-middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
+const isAuthenticated = (request : string) => {
+  const protectedPaths = ["/login","/signup"];
+  return protectedPaths.includes(request);
+}
+
+
+
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   if (!token) return NextResponse.redirect(new URL("/login", request.url));
-  console.log(token);
+  if(isAuthenticated(request.nextUrl.pathname)) return NextResponse.redirect(new URL("/", request.url));
   return token.isAdmin
     ? NextResponse.next()
     : NextResponse.redirect(new URL("/explore/rooms", request.url));
 }
 
 export const config = {
-    matcher : ["/admin/:path*"]
+    matcher : ["/login","/signup","/admin/:path*"]
 }
